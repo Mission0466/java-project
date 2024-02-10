57831916a648da530f63ad085642912257a44da5
Krishna Chaitanya Chittapragada <101656984+Mission0466@users.noreply.github.com>
2024-02-10 20:28:20 +0530

    added new method in hello java

--- hello.java
+++ hello.java
@@ -3,4 +3,7 @@ public class hello{
     public static void main(String[] args) {
         System.out.println("hello world!@@@@@@@@@@");
     }
+    public static void display(String[] args) {
+        System.out.println("hello world!@@@@@@@@@@");
+    }
 }
